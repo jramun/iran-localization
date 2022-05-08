c31b47e2fafe5b9928d67a4b15c805ed1b260cdb
Mojtaba (Ramun) Sharif <jramun7@gmail.com>
2022-05-08 23:10:36 +0430

    Repository is changed. Main is changed.

--- manipulate/main.py
+++ manipulate/main.py
@@ -5,6 +5,7 @@ import model
 from model import Province, County, Area, Village, LittleVillage
 from repository import Repository
 
+# region keys
 k_p_local_id = "OSTAN"
 k_p_name = "نام استان"
 k_co_local_id = "SHAHRESTAN"
@@ -18,8 +19,9 @@ k_code_rec = "CODEREC"
 k_lv_diag = "DIAG"
 k_name = "NAME"
 sheet = 'فایل تقسیمات کشوری 99'
-session = None
+# endregion
 repository = None
+engine = None
 
 
 def handler():
@@ -27,123 +29,117 @@ def handler():
                              usecols=[k_name, k_p_local_id, k_p_name, k_co_local_id, k_co_name, k_a_local_id, k_a_name,
                                       k_v_local_id, k_v_name, k_lv_local_id, k_lv_diag, k_code_rec])
     dic = data.to_dict(orient='records')
+    counter = 0
     for item in dic:
+        counter += 1
         if is_little_village(item) is True:
+            print(counter, 'Little village ', item)
             addLittleVillage(item)
             continue
         if is_village(item) is True:
+            print(counter, 'Village ', item)
             addVillage(item)
             continue
         if is_area(item) is True:
+            print(counter, 'Area ', item)
             addArea(item)
             continue
         if is_county(item) is True:
+            print(counter, 'County ', item)
             addCounty(item)
             continue
         if is_province(item) is True:
+            print(counter, 'Province ', item)
             addProvince(item)
             continue
-
-    session.commit()
-    session.flush()
-    session.close()
     return dic
 
 
 def addLittleVillage(item) -> LittleVillage:
-    province = getProvince(item)
+    repository = Repository(engine)
+    province = Province(name=item[k_p_name],
+                        local_id=str(item[k_p_local_id]))
     province = repository.addProvince(province)
-    county = getCounty(item)
+    county = County(name=item[k_co_name],
+                    local_id=str(item[k_co_local_id]))
     county.province = province
     county = repository.addCounty(county)
-    area = getArea(item)
+    area = Area(name=item[k_a_name],
+                local_id=str(item[k_a_local_id]))
     area.county = county
     area = repository.addArea(area)
-    village = getVillage(item)
+    village = Village(name=item[k_v_name],
+                      local_id=str(item[k_v_local_id]))
     village.area = area
     village = repository.addVillage(village)
-    littleVillage = getLittleVillage(item)
+    littleVillage = LittleVillage(name=item[k_name],
+                                  local_id=str(item[k_lv_local_id]),
+                                  code_rec=item[k_code_rec],
+                                  diag=item[k_lv_diag])
     littleVillage.village = village
-    littleVillage = repository.addLittleVillage(littleVillage)
+    littleVillage = repository.updateLittleVillage(littleVillage)
     return littleVillage
 
 
 def addVillage(item) -> Village:
-    province = getProvince(item)
+    repository = Repository(engine)
+    province = Province(name=item[k_p_name],
+                        local_id=str(item[k_p_local_id]))
     province = repository.addProvince(province)
-    county = getCounty(item)
+    county = County(name=item[k_co_name],
+                    local_id=str(item[k_co_local_id]))
     county.province = province
     county = repository.addCounty(county)
-    area = getArea(item)
+    area = Area(name=item[k_a_name],
+                local_id=str(item[k_a_local_id]))
     area.county = county
     area = repository.addArea(area)
-    village = getVillage(item)
+    village = Village(name=item[k_name],
+                      local_id=str(item[k_v_local_id]),
+                      code_rec=item[k_code_rec])
     village.area = area
-    village = repository.addVillage(village)
+    village = repository.updateVillage(village)
     return village
 
 
 def addArea(item) -> Area:
-    province = getProvince(item)
+    repository = Repository(engine)
+    province = Province(name=item[k_p_name],
+                        local_id=str(item[k_p_local_id]))
     province = repository.addProvince(province)
-    county = getCounty(item)
+    county = County(name=item[k_co_name],
+                    local_id=str(item[k_co_local_id]))
     county.province = province
     county = repository.addCounty(county)
-    area = getArea(item)
+    area = Area(name=item[k_name],
+                local_id=str(item[k_a_local_id]),
+                code_rec=item[k_code_rec])
     area.county = county
-    area = repository.addArea(area)
+    area = repository.updateArea(area)
     return area
 
 
 def addCounty(item) -> County:
-    province = getProvince(item)
+    repository = Repository(engine)
+    province = Province(name=item[k_p_name],
+                        local_id=str(item[k_p_local_id]))
     province = repository.addProvince(province)
-    county = getCounty(item)
+    county = County(name=item[k_name],
+                    local_id=str(item[k_co_local_id]),
+                    code_rec=item[k_code_rec])
     county.province = province
-    county = repository.addCounty(county)
+    county = repository.updateCounty(county)
     return county
 
 
 def addProvince(item) -> Province:
-    province = getProvince(item)
-    province = repository.addProvince(province)
+    repository = Repository(engine)
+    province = Province(name=item[k_name],
+                        local_id=str(item[k_p_local_id]),
+                        code_rec=item[k_code_rec])
+    province = repository.updateProvince(province)
     return province
 
-
-# region make model
-
-def getProvince(item) -> Province:
-    return Province(name=item[k_p_name],
-                    local_id=str(item[k_p_local_id]),
-                    code_rec=item[k_code_rec])
-
-
-def getCounty(item) -> County:
-    return County(name=item[k_co_name],
-                  local_id=str(item[k_co_local_id]),
-                  code_rec=item[k_code_rec])
-
-
-def getArea(item) -> Area:
-    return Area(name=item[k_a_name],
-                local_id=str(item[k_a_local_id]),
-                code_rec=item[k_code_rec])
-
-
-def getVillage(item) -> Village:
-    return Village(name=item[k_v_name],
-                   local_id=str(item[k_v_local_id]),
-                   code_rec=item[k_code_rec])
-
-
-def getLittleVillage(item) -> LittleVillage:
-    return LittleVillage(name=item[k_name],
-                         local_id=str(item[k_lv_local_id]),
-                         diag=item[k_lv_diag],
-                         code_rec=item[k_code_rec])
-
-
-# endregion
 
 # region validation
 def is_little_village(item):
@@ -180,6 +176,6 @@ def is_province(item):
 
 
 if __name__ == '__main__':
-    session = Session(model.engine())
-    repository = Repository(session)
+
+    engine = model.engine()
     values = handler()
--- manipulate/repository.py
+++ manipulate/repository.py
@@ -1,46 +1,182 @@
+from sqlalchemy.orm import Session
+
 from model import Province, County, Area, Village, LittleVillage
 
 
+def replaceProvince(source, target):
+    source.name = target.name
+    source.code_rec = target.code_rec
+    return source
+
+
+def replaceCounty(source, target):
+    source.name = target.name
+    source.code_rec = target.code_rec
+    return source
+
+
+def replaceArea(source, target):
+    source.name = target.name
+    source.code_rec = target.code_rec
+    return source
+
+
+def replaceVillage(source, target):
+    source.name = target.name
+    source.code_rec = target.code_rec
+    return source
+
+
+def replaceLittleVillage(source, target):
+    source.name = target.name
+    source.code_rec = target.code_rec
+    source.diag = target.diag
+    return source
+
+
 class Repository:
-    def __init__(self, session):
-        self.session = session
+    def __init__(self, engine):
+        self.session = None
+        self.engine = engine
+
+    def open(self):
+        self.session = Session(self.engine)
+
+    def commit(self):
+        self.session.commit()
+        self.session.flush()
+        self.session.close()
+
+    def findProvince(self, localId) -> Province:
+        self.open()
+        object = self.session.query(Province).filter_by(local_id=localId).first()
+        return object
 
     def addProvince(self, province) -> Province:
+        self.open()
         object = self.session.query(Province).filter_by(local_id=province.local_id).first()
         if object is None:
             self.session.add(province)
-            return province
+            result = province
         else:
-            return object
+            result = object
+        self.commit()
+        return result
+
+    def updateProvince(self, province) -> Province:
+        self.open()
+        object = self.session.query(Province).filter_by(local_id=province.local_id).first()
+        if object is not None:
+            result = self.session.add(replaceProvince(object, province))
+        else:
+            result = self.session.add(province)
+        self.commit()
+        return result
+
+    def findCounty(self, localId) -> County:
+        self.open()
+        object = self.session.query(County).filter_by(local_id=localId).first()
+        self.commit()
+        return object
 
     def addCounty(self, county) -> County:
+        self.open()
         object = self.session.query(County).filter_by(local_id=county.local_id).first()
         if object is None:
             self.session.add(county)
-            return county
+            result = county
         else:
-            return object
+            result = object
+        self.commit()
+        return result
+
+    def updateCounty(self, county) -> County:
+        self.open()
+        object = self.session.query(County).filter_by(local_id=county.local_id).first()
+        if object is not None:
+            result = self.session.add(replaceCounty(object, county))
+        else:
+            result = self.session.add(county)
+        self.commit()
+        return result
+
+    def findArea(self, localId) -> Area:
+        self.open()
+        object = self.session.query(Area).filter_by(local_id=localId).first()
+        self.commit()
+        return object
 
     def addArea(self, area) -> Area:
+        self.open()
         object = self.session.query(Area).filter_by(local_id=area.local_id).first()
         if object is None:
             self.session.add(area)
-            return area
+            result = area
         else:
-            return object
+            result = object
+        self.commit()
+        return result
+
+    def updateArea(self, area) -> Area:
+        self.open()
+        object = self.session.query(Area).filter_by(local_id=area.local_id).first()
+        if object is not None:
+            result = self.session.add(replaceArea(object, area))
+        else:
+            result = self.session.add(area)
+        self.commit()
+        return result
 
     def addVillage(self, village) -> Village:
+        self.open()
         object = self.session.query(Village).filter_by(local_id=village.local_id).first()
         if object is None:
             self.session.add(village)
-            return village
+            result = village
         else:
-            return object
+            result = object
+        self.commit()
+        return result
+
+    def findVillage(self, localId) -> Village:
+        self.open()
+        object = self.session.query(Village).filter_by(local_id=localId).first()
+        self.commit()
+        return object
+
+    def updateVillage(self, village) -> Village:
+        self.open()
+        object = self.session.query(Village).filter_by(local_id=village.local_id).first()
+        if object is not None:
+            result = self.session.add(replaceVillage(object, village))
+        else:
+            result = self.session.add(village)
+        self.commit()
+        return result
 
     def addLittleVillage(self, littleVillage) -> LittleVillage:
+        self.open()
         object = self.session.query(LittleVillage).filter_by(local_id=littleVillage.local_id).first()
         if object is None:
             self.session.add(littleVillage)
-            return littleVillage
+            result = littleVillage
         else:
-            return object
+            result = object
+        self.commit()
+        return result
+
+    def updateLittleVillage(self, littleVillage) -> LittleVillage:
+        self.open()
+        object = self.session.query(LittleVillage).filter_by(local_id=littleVillage.local_id).first()
+        if object is not None:
+            result = self.session.add(replaceLittleVillage(object, littleVillage))
+        else:
+            result = self.session.add(littleVillage)
+        self.commit()
+        return result
+
+    def findLittleVillage(self, localId) -> LittleVillage:
+        self.open()
+        object = self.session.query(LittleVillage).filter_by(local_id=localId).first()
+        self.commit()
+        return object
